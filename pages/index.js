5dee3c777b609cf83a02d769835d335ab6954533
Diti2604 <ditiboyboy@gmail.com>
2023-11-24 13:41:51 +0100

    create the widget component and the search bar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Sidebar from "../components/Sidebar";
 import Feed from "../components/Feed";
+import Widgets from "../components/Widgets";
 
 export default function Home() {
   return (
@@ -11,17 +12,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-<main className="flex min-h-screen max-w-7xl mx-auto ">
-   {/* Sidebar */}
-      <Sidebar />
-      {/* Feed */}
-      <Feed/>
+      <main className="flex min-h-screen max-w-7xl mx-auto ">
+        {/* Sidebar */}
+        <Sidebar />
+        {/* Feed */}
+        <Feed />
 
-      {/* Widgets */}
-
-      {/* Modal */}
-</main>
-     
+        {/* Widgets */}
+        <Widgets />
+        {/* Modal */}
+      </main>
     </div>
   );
 }

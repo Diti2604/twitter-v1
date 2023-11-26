d1ff065ae8a3bdd94a58fa9b4dae3ee8670ede16
Diti2604 <ditiboyboy@gmail.com>
2023-11-26 23:28:57 +0100

    get the comments from firestore

--- components/Widgets.js
+++ components/Widgets.js
@@ -23,7 +23,7 @@ export default function Widgets({ newsResult, randomUsersResults }) {
       <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
         <h4 className="font-bold text-xl px-4 ">What&apos;s happening</h4>
         <AnimatePresence>
-          {newsResult.slice(0, articleNum).map((article) => (
+          {newsResult?.slice(0, articleNum).map((article) => (
             <motion.div
               key={article.title}
               initial={{ opacity: 0 }}
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,24 +1,48 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import { ArrowLeftIcon } from "@heroicons/react/outline";
 import Head from "next/head";
+import CommentModal from "../../components/CommentModal";
 import Sidebar from "../../components/Sidebar";
 import Widgets from "../../components/Widgets";
 import Post from "../../components/Post";
-import CommentModal from "../../components/CommentModal";
-import { ArrowLeftIcon } from "@heroicons/react/outline";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { doc, onSnapshot } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { db } from "../../firebase";
+import { AnimatePresence, motion } from "framer-motion";
+import Comment from "../../components/comment";
 
-export default function PostPage({ newsResult, randomUsersResults }) {
+export default function PostPage({ newsResults, randomUsersResults }) {
   const router = useRouter();
   const { id } = router.query;
   const [post, setPost] = useState();
+  const [comments, setComments] = useState([]);
+
+  // get the post data
 
   useEffect(
     () => onSnapshot(doc(db, "posts", id), (snapshot) => setPost(snapshot)),
     [db, id]
   );
+
+  // get comments of the post
+
+  useEffect(() => {
+    onSnapshot(
+      query(
+        collection(db, "posts", id, "comments"),
+        orderBy("timestamp", "desc")
+      ),
+      (snapshot) => setComments(snapshot.docs)
+    );
+  }, [db, id]);
+
   return (
     <div>
       <Head>
@@ -27,12 +51,14 @@ export default function PostPage({ newsResult, randomUsersResults }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex min-h-screen mx-auto ">
+      <main className="flex min-h-screen mx-auto">
         {/* Sidebar */}
         <Sidebar />
+
         {/* Feed */}
+
         <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
-          <div className="flex items-center space-x-2 py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
+          <div className="flex items-center space-x-2  py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
             <div className="hoverEffect" onClick={() => router.push("/")}>
               <ArrowLeftIcon className="h-5 " />
             </div>
@@ -40,14 +66,39 @@ export default function PostPage({ newsResult, randomUsersResults }) {
               Tweet
             </h2>
           </div>
+
           <Post id={id} post={post} />
+          {comments.length > 0 && (
+            <div className="">
+              <AnimatePresence>
+                {comments.map((comment) => (
+                  <motion.div
+                    key={comment.id}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 1 }}
+                  >
+                    <Comment
+                      key={comment.id}
+                      commentId={comment.id}
+                      originalPostId={id}
+                      comment={comment.data()}
+                    />
+                  </motion.div>
+                ))}
+              </AnimatePresence>
+            </div>
+          )}
         </div>
 
         {/* Widgets */}
+
         <Widgets
-          newsResult={newsResult.articles}
+          newsResults={newsResults.articles}
           randomUsersResults={randomUsersResults.results}
         />
+
         {/* Modal */}
 
         <CommentModal />
@@ -56,21 +107,30 @@ export default function PostPage({ newsResult, randomUsersResults }) {
   );
 }
 
-//https://saurav.tech/NewsAPI/top-headlines/category/business/us.json
+// https://saurav.tech/NewsAPI/top-headlines/category/business/us.json
 
 export async function getServerSideProps() {
-  const newsResult = await fetch(
+  const newsResults = await fetch(
     "https://saurav.tech/NewsAPI/top-headlines/category/business/us.json"
   ).then((res) => res.json());
 
-  //Who to follow section
+  // Who to follow section
 
-  const randomUsersResults = await fetch(
-    "https://randomuser.me/api/?results=30&inc=name,login,picture"
-  ).then((res) => res.json());
+  let randomUsersResults = [];
+
+  try {
+    const res = await fetch(
+      "https://randomuser.me/api/?results=30&inc=name,login,picture"
+    );
+
+    randomUsersResults = await res.json();
+  } catch (e) {
+    randomUsersResults = [];
+  }
+
   return {
     props: {
-      newsResult,
+      newsResults,
       randomUsersResults,
     },
   };

9717b5a2c35cf1798998511ecdd80e661f9da333
Diti2604 <ditiboyboy@gmail.com>
2023-11-26 18:09:10 +0100

    get post data from firestore and show them postsec

--- components/Post.js
+++ components/Post.js
@@ -6,6 +6,7 @@ import {
   ShareIcon,
   TrashIcon,
 } from "@heroicons/react/outline";
+import Moment from 'react-moment';
 
 /* eslint-disable @next/next/no-img-element */
 export default function Post({ post }) {
@@ -13,7 +14,7 @@ export default function Post({ post }) {
     <div className="flex p-3 cursor-pointer border-b border-gray-200 ">
       {/* user image */}
       <img
-        src={post.userImg}
+        src={post.data().userImg}
         alt="user-img"
         className="h-11 w-11 rounded-full   mr-4"
       />
@@ -25,19 +26,19 @@ export default function Post({ post }) {
           {/*  post user info */}
 
           <div className=" flex space-x-1  whitespace-nowrap items-center">
-            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">{post.name}</h4>
-            <span className="text-sm sm:text-[15px]">@{post.username} - </span>
-            <span className="text-sm sm:text-[15px] hover:underline ">{post.timestamps}</span>
+            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">{post.data().name}</h4>
+            <span className="text-sm sm:text-[15px]">@{post.data().username} - </span>
+            <span className="text-sm sm:text-[15px] hover:underline "><Moment fromNow>{post?.timestamps?.toDate()}</Moment></span>
           </div>
           {/* dot icon */}
           <DotsHorizontalIcon className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2 " />
         </div>
 
         {/* post text */}
-        <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2 ">{post.text}</p>
+        <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2 ">{post.data().text}</p>
 
         {/* image */}
-        <img src={post.img} alt="pic" className="rounded-2xl mr-2 "></img>
+        <img src={post.data().image} alt="pic" className="rounded-2xl mr-2 "></img>
         {/* post image */}
 
         {/* icons */}

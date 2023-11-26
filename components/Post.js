9e9de1cf2025a71de94cb04498d2f6909a563907
Diti2604 <ditiboyboy@gmail.com>
2023-11-26 23:08:35 +0100

    create post page

--- components/Post.js
+++ components/Post.js
@@ -6,7 +6,6 @@ import {
   HeartIcon,
   ShareIcon,
   TrashIcon,
-  TrendingUpIcon,
 } from "@heroicons/react/outline";
 import {
   collection,
@@ -23,25 +22,27 @@ import { useState, useEffect } from "react";
 import { deleteObject, ref } from "firebase/storage";
 import { useRecoilState } from "recoil";
 import { modalState, postIdState } from "../atom/modalAtom";
+import { useRouter } from "next/router";
 
 /* eslint-disable @next/next/no-img-element */
-export default function Post({ post }) {
+export default function Post({ post, id }) {
   const { data: session } = useSession();
   const [likes, setLikes] = useState([]);
   const [comments, setComments] = useState([]);
   const [hasLiked, setHasLiked] = useState(false);
   const [open, setOpen] = useRecoilState(modalState);
   const [postId, setPostId] = useRecoilState(postIdState);
+  const router = useRouter();
 
   useEffect(() => {
     const unsubscribe = onSnapshot(
-      collection(db, "posts", post.id, "likes"),
+      collection(db, "posts", id, "likes"),
       (snapshot) => setLikes(snapshot.docs)
     );
   }, [db]);
   useEffect(() => {
     const unsubscribe = onSnapshot(
-      collection(db, "posts", post.id, "comments"),
+      collection(db, "posts", id, "comments"),
       (snapshot) => setComments(snapshot.docs)
     );
   }, [db]);
@@ -55,9 +56,9 @@ export default function Post({ post }) {
   async function likePost() {
     if (session) {
       if (hasLiked) {
-        await deleteDoc(doc(db, "posts", post.id, "likes", session?.user.uid));
+        await deleteDoc(doc(db, "posts", id, "likes", session?.user.uid));
       } else {
-        await setDoc(doc(db, "posts", post.id, "likes", session?.user.uid), {
+        await setDoc(doc(db, "posts", id, "likes", session?.user.uid), {
           username: session.user.username,
         });
       }
@@ -68,10 +69,11 @@ export default function Post({ post }) {
 
   async function deletePost() {
     if (window.confirm("Are you sure you want to delete this post?")) {
-      deleteDoc(doc(db, "posts", post.id));
-      if (post.data().image) {
-        deleteObject(ref(storage, `posts/${post.id}/image`));
+      deleteDoc(doc(db, "posts", id));
+      if (post?.data().image) {
+        deleteObject(ref(storage, `posts/${id}/image`));
       }
+      router.push("/")
     }
   }
 
@@ -79,7 +81,7 @@ export default function Post({ post }) {
     <div className="flex p-3 cursor-pointer border-b border-gray-200 ">
       {/* user image */}
       <img
-        src={post.data().userImg}
+        src={post?.data()?.userImg}
         alt="user-img"
         className="h-11 w-11 rounded-full   mr-4"
       />
@@ -92,13 +94,13 @@ export default function Post({ post }) {
 
           <div className=" flex space-x-1  whitespace-nowrap items-center">
             <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
-              {post.data().name}
+              {post?.data()?.name}
             </h4>
             <span className="text-sm sm:text-[15px]">
-              @{post.data().username} -{" "}
+              @{post?.data()?.username} -{" "}
             </span>
             <span className="text-sm sm:text-[15px] hover:underline ">
-              <Moment fromNow>{post?.data().timestamp?.toDate()}</Moment>
+              <Moment fromNow>{post?.data()?.timestamp?.toDate()}</Moment>
             </span>
           </div>
           {/* dot icon */}
@@ -107,12 +109,12 @@ export default function Post({ post }) {
 
         {/* post text */}
         <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2 ">
-          {post.data().text}
+          {post?.data()?.text}
         </p>
 
         {/* image */}
         <img
-          src={post.data().image}
+          src={post?.data()?.image}
           alt="pic"
           className="rounded-2xl mr-2 "
         ></img>
@@ -127,17 +129,17 @@ export default function Post({ post }) {
                 if (!session) {
                   signIn();
                 } else {
-                  setPostId(post.id);
+                  setPostId(id);
                   setOpen(!open);
                 }
               }}
               className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"
             />
-            {comments.length>0 && (
+            {comments.length > 0 && (
               <span className="text-sm">{comments.length}</span>
             )}
           </div>
-          {session?.user.uid === post?.data().id && (
+          {session?.user.uid === post?.data()?.id && (
             <TrashIcon
               onClick={deletePost}
               className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100"

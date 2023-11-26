2852598f003acbc4deeab1d1417fbf544839161f
Diti2604 <ditiboyboy@gmail.com>
2023-11-26 23:55:57 +0100

    complete the comment component

--- components/Post.js
+++ components/Post.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react-hooks/exhaustive-deps */
 import {
   ChartBarIcon,
@@ -7,6 +8,8 @@ import {
   ShareIcon,
   TrashIcon,
 } from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import Moment from "react-moment";
 import {
   collection,
   deleteDoc,
@@ -14,8 +17,6 @@ import {
   onSnapshot,
   setDoc,
 } from "firebase/firestore";
-import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
-import Moment from "react-moment";
 import { db, storage } from "../firebase";
 import { signIn, useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
@@ -23,8 +24,6 @@ import { deleteObject, ref } from "firebase/storage";
 import { useRecoilState } from "recoil";
 import { modalState, postIdState } from "../atom/modalAtom";
 import { useRouter } from "next/router";
-
-/* eslint-disable @next/next/no-img-element */
 export default function Post({ post, id }) {
   const { data: session } = useSession();
   const [likes, setLikes] = useState([]);
@@ -33,7 +32,6 @@ export default function Post({ post, id }) {
   const [open, setOpen] = useRecoilState(modalState);
   const [postId, setPostId] = useRecoilState(postIdState);
   const router = useRouter();
-
   useEffect(() => {
     const unsubscribe = onSnapshot(
       collection(db, "posts", id, "likes"),
@@ -46,13 +44,11 @@ export default function Post({ post, id }) {
       (snapshot) => setComments(snapshot.docs)
     );
   }, [db]);
-
   useEffect(() => {
     setHasLiked(
       likes.findIndex((like) => like.id === session?.user.uid) !== -1
     );
   }, [likes]);
-
   async function likePost() {
     if (session) {
       if (hasLiked) {
@@ -66,40 +62,36 @@ export default function Post({ post, id }) {
       signIn();
     }
   }
-
   async function deletePost() {
     if (window.confirm("Are you sure you want to delete this post?")) {
       deleteDoc(doc(db, "posts", id));
-      if (post?.data().image) {
+      if (post.data().image) {
         deleteObject(ref(storage, `posts/${id}/image`));
       }
-      router.push("/")
+      router.push("/");
     }
   }
-
   return (
-    <div className="flex p-3 cursor-pointer border-b border-gray-200 ">
+    <div className="flex p-3 cursor-pointer border-b border-gray-200">
       {/* user image */}
       <img
+        className="h-11 w-11 rounded-full mr-4"
         src={post?.data()?.userImg}
         alt="user-img"
-        className="h-11 w-11 rounded-full   mr-4"
       />
       {/* right side */}
       <div className="flex-1">
         {/* Header */}
-
         <div className="flex items-center justify-between">
-          {/*  post user info */}
-
-          <div className=" flex space-x-1  whitespace-nowrap items-center">
+          {/* post user info */}
+          <div className="flex items-center space-x-1 whitespace-nowrap">
             <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
               {post?.data()?.name}
             </h4>
             <span className="text-sm sm:text-[15px]">
               @{post?.data()?.username} -{" "}
             </span>
-            <span className="text-sm sm:text-[15px] hover:underline ">
+            <span className="text-sm sm:text-[15px] hover:underline">
               <Moment fromNow>{post?.data()?.timestamp?.toDate()}</Moment>
             </span>
           </div>
@@ -108,21 +100,26 @@ export default function Post({ post, id }) {
         </div>
 
         {/* post text */}
-        <p className="text-gray-800 text-[15px] sm:text-[16px] mb-2 ">
+
+        <p
+          onClick={() => router.push(`/posts/${id}`)}
+          className="text-gray-800 text-[15px sm:text-[16px] mb-2"
+        >
           {post?.data()?.text}
         </p>
 
-        {/* image */}
-        <img
-          src={post?.data()?.image}
-          alt="pic"
-          className="rounded-2xl mr-2 "
-        ></img>
         {/* post image */}
+
+        <img
+          onClick={() => router.push(`/posts/${id}`)}
+          className="rounded-2xl mr-2"
+          src={post?.data()?.image}
+          alt=""
+        />
 
         {/* icons */}
 
-        <div className="flex justify-between text-gray-500 p-2 ">
+        <div className="flex justify-between text-gray-500 p-2">
           <div className="flex items-center select-none">
             <ChatIcon
               onClick={() => {
@@ -145,7 +142,6 @@ export default function Post({ post, id }) {
               className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100"
             />
           )}
-
           <div className="flex items-center">
             {hasLiked ? (
               <HeartIconFilled
@@ -162,11 +158,11 @@ export default function Post({ post, id }) {
               <span
                 className={`${hasLiked && "text-red-600"} text-sm select-none`}
               >
+                {" "}
                 {likes.length}
               </span>
             )}
           </div>
-
           <ShareIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
           <ChartBarIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
         </div>

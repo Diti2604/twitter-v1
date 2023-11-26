b6f507dd21730feebf371e34ed9bba353469c5d3
Diti2604 <ditiboyboy@gmail.com>
2023-11-26 22:32:45 +0100

    send comments to firestore and show the num of com

--- components/Post.js
+++ components/Post.js
@@ -28,6 +28,7 @@ import { modalState, postIdState } from "../atom/modalAtom";
 export default function Post({ post }) {
   const { data: session } = useSession();
   const [likes, setLikes] = useState([]);
+  const [comments, setComments] = useState([]);
   const [hasLiked, setHasLiked] = useState(false);
   const [open, setOpen] = useRecoilState(modalState);
   const [postId, setPostId] = useRecoilState(postIdState);
@@ -36,6 +37,12 @@ export default function Post({ post }) {
     const unsubscribe = onSnapshot(
       collection(db, "posts", post.id, "likes"),
       (snapshot) => setLikes(snapshot.docs)
+    );
+  }, [db]);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, "posts", post.id, "comments"),
+      (snapshot) => setComments(snapshot.docs)
     );
   }, [db]);
 
@@ -77,7 +84,7 @@ export default function Post({ post }) {
         className="h-11 w-11 rounded-full   mr-4"
       />
       {/* right side */}
-      <div className="">
+      <div className="flex-1">
         {/* Header */}
 
         <div className="flex items-center justify-between">
@@ -114,17 +121,22 @@ export default function Post({ post }) {
         {/* icons */}
 
         <div className="flex justify-between text-gray-500 p-2 ">
-          <ChatIcon
-            onClick={() => {
-              if (!session) {
-                signIn();
-              } else {
-                setPostId(post.id);
-                setOpen(!open);
-              }
-            }}
-            className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"
-          />
+          <div className="flex items-center select-none">
+            <ChatIcon
+              onClick={() => {
+                if (!session) {
+                  signIn();
+                } else {
+                  setPostId(post.id);
+                  setOpen(!open);
+                }
+              }}
+              className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"
+            />
+            {comments.length>0 && (
+              <span className="text-sm">{comments.length}</span>
+            )}
+          </div>
           {session?.user.uid === post?.data().id && (
             <TrashIcon
               onClick={deletePost}

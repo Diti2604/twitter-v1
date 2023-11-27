abfc7cefd1a6cd6b9cc3e2c6573dd3e97d51081f
Diti2604 <ditiboyboy@gmail.com>
2023-11-27 18:08:36 +0100

    and one more

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
@@ -10,12 +10,15 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+
   pages: {
     signIn: "/auth/signin",
   },
+
   secret: process.env.SECRET,
+
   callbacks: {
-    async session({ session, token}) {
+    async session({ session, token }) {
       session.user.username = session.user.name
         .split(" ")
         .join("")
@@ -24,6 +27,4 @@ export const authOptions = {
       return session;
     },
   },
-};
-
-export default NextAuth(authOptions);
+});

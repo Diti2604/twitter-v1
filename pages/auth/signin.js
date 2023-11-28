82470f5e2fa42a7ee8d1f1cbae1451c168f3c245
Diti2604 <ditiboyboy@gmail.com>
2023-11-28 20:21:30 +0100

    small signin change

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,20 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
 import { db } from "../../firebase";
-import {
-  getAuth,
-  signInWithPopup,
-  GoogleAuthProvider,
-  signInWithRedirect,
-} from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
-import { getProviders, signIn } from "next-auth/react";
 import { useRouter } from "next/router";
-//montihfdnv
-
-export default function Signin({ providers }) {
+export default function Signin() {
   const router = useRouter();
-  //done
-  const handleSignIn = async (providerId) => {
+  const onGoogleClick = async () => {
     try {
       const auth = getAuth();
       const provider = new GoogleAuthProvider();
@@ -36,7 +27,7 @@ export default function Signin({ providers }) {
     } catch (error) {
       console.log(error);
     }
-  }
+  };
   return (
     <div className="flex justify-center mt-20 space-x-4">
       <img
@@ -45,34 +36,23 @@ export default function Signin({ providers }) {
         className="hidden object-cover md:w-44 md:h-80 rotate-6  md:inline-flex"
       />
       <div className="">
-        {Object.values(providers).map((provider) => (
-          <div className="flex flex-col items-center" key={provider.id}>
-            <img
-              className="w-36 object-cover"
-              src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
-              alt="twitter logo"
-            />
-            <p className="text-center text-sm italic my-10">
-              This app is created for learning purposes
-            </p>
-            <button
-              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
-              className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500"
-            >
-              Sign in with {provider.name}
-            </button>
-          </div>
-        ))}
+        <div className="flex flex-col items-center">
+          <img
+            className="w-36 object-cover"
+            src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
+            alt="twitter logo"
+          />
+          <p className="text-center text-sm italic my-10">
+            This app is created for learning purposes
+          </p>
+          <button
+            onClick={onGoogleClick}
+            className="bg-red-400 rounded-lg p-3 text-white hover:bg-red-500"
+          >
+            Sign in with Google
+          </button>
+        </div>
       </div>
     </div>
   );
-};
-
-export async function getServerSideProps() {
-  const providers = await getProviders();
-  return {
-    props: {
-      providers,
-    },
-  };
 }
